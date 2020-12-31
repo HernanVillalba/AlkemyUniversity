a95a8d784318d4851e9444b01d87f2aec759ceaa
hernanVillalba <63215609+hernanVillalba@users.noreply.github.com>
2020-12-31 01:38:50 -0300

    Readme
    subida de archivo README y convertí el script de la creación de la base de datos en 1 solo archivo.

--- AlkemyUniversity/Database/Script_DB-AlkemyUniversity.sql
+++ AlkemyUniversity/Database/Script_DB-AlkemyUniversity.sql
@@ -1,12 +1,11 @@
-/*
-use master
-drop database AlkemyUniversity
-*/
-
 CREATE DATABASE AlkemyUniversity
+
 GO
+
 USE AlkemyUniversity
+
 GO
+
 CREATE TABLE Subjects(
 ID int not null identity(1,1),
 Subject_Name varchar(100) not null unique,
@@ -136,40 +135,433 @@ ALTER TABLE Teachers_by_Subject
 ADD CONSTRAINT FK_TeacBySubj_IDTeacher FOREIGN KEY(Teacher_ID) REFERENCES Teachers(ID)
 
 -----------------------------------------------------------------------------------------
---INSERT DEL PRIMER ADMINISTRADOR PARA LA PAGINA--
+GO
+CREATE PROCEDURE SP_Login_Admin(
+@id int,
+@password varchar(100)
+)
+AS
+BEGIN
+	select U.ID, U.Administrator from Administrator as A
+	join Users as U on U.ID=A.User_ID
+	where A.User_ID=@id and A.Password=@password and U.Administrator=1;
+END
+
+GO
+
+CREATE PROCEDURE SP_New_Teacher(
+@dni int,
+@lastName varchar(200),
+@names varchar(200),
+@active bit
+)
+AS
+BEGIN 
+	BEGIN TRY
+		INSERT INTO Teachers(DNI,Lastname, Names,Active)
+		VALUES (@dni,@lastName,@names,@active)
+	END TRY
+	BEGIN CATCH
+		RAISERROR('No se pudo agregar al profesor en la base de datos.',16,1);
+	END CATCH
+END	
+
+GO
+
+CREATE VIEW VW_Ative_Teachers
+AS
+select T.ID, T.Lastname, t.Names, T.DNI, T.Active,
+T.Lastname +' '+ T.Names as 'FullName'
+from Teachers as T where Active=1
+
+GO
+
+CREATE PROCEDURE SP_New_Subject(
+@subject_name varchar(100),
+@maximum_capacity int,
+@day varchar(100),
+@start_time time,
+@end_time time,
+@teacher_id int
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			INSERT INTO Subjects(Subject_Name, Maximum_Capacity, Places_Available)
+			VALUES(@subject_name, @maximum_capacity, @maximum_capacity)
+			DECLARE @ultID int
+			SET @ultID = @@IDENTITY
+
+			INSERT INTO Schedules(Subject_ID,Day,Start_Time,End_Time)
+			VALUES(@ultID, @day, @start_time, @end_time)
+
+			INSERT INTO Teachers_by_Subject(Subject_ID,Teacher_ID)
+			VALUES(@ultID, @teacher_id)
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+		RAISERROR('No se pudo agregar la materia.',16,1);
+	END CATCH
+
+END
+
+GO
+
+CREATE VIEW VW_Subject_Info
+AS
+SELECT ISNULL(S.ID, 0) AS Subject_ID,
+	   ISNULL(S.Subject_Name, '---') AS Subject_Name,
+	   ISNULL(S.Places_Available,ISNULL(S.Maximum_Capacity,0)) AS Places_Availables,
+	   ISNULL(S.Maximum_Capacity,0) AS Maximum_Capacity,
+	   ISNULL(T.ID, 0) AS Teacher_ID,
+	   ISNULL(T.Lastname, '---') AS Lastname, 
+	   ISNULL(T.Names, '---') AS Names, 
+	   ISNULL(SCH.Day, '---') AS Day,
+	   ISNULL(SCH.Start_Time,'00:00') AS Start_Time,
+	   ISNULL(SCH.End_Time,'00:00') AS End_Time
+FROM Subjects AS S
+full join Teachers_by_Subject AS tbs ON TBS.Subject_ID = S.ID
+left join Teachers AS T ON T.ID = TBS.Teacher_ID
+left join Schedules AS SCH ON SCH.Subject_ID = S.ID
+
+GO
+
+CREATE PROCEDURE SP_Subject_Search(
+@keyword varchar(100)
+)
+AS
+BEGIN
+	SELECT * FROM VW_Subject_Info AS VW
+	WHERE VW.Subject_Name like '%'+ @keyword + '%' OR
+		  VW.Lastname like '%'+ @keyword + '%' or
+		  VW.Names like '%'+ @keyword + '%' OR
+		  VW.Day like '%'+ @keyword + '%'
+END
+
+GO
+
+CREATE VIEW VW_List_Teachers
+AS
+SELECT T.ID,T.Lastname,T.Names,T.DNI,T.Active,
+(
+SELECT count(TBS.Teacher_ID) FROM Teachers_by_Subject AS TBS
+WHERE TBS.Teacher_ID = T.ID
+) AS 'Cantidad de materias'
+FROM Teachers AS T
+
+GO
+
+CREATE PROCEDURE SP_Teachers_Search(
+@keyword varchar(100)
+)
+AS
+BEGIN
+	SELECT*FROM VW_List_Teachers AS VW
+	WHERE VW.Lastname LIKE '%'+@keyword+'%' OR
+	VW.Names LIKE '%'+@keyword+'%' OR
+	VW.DNI LIKE '%'+@keyword+'%'
+END
+
+GO
+
+CREATE PROCEDURE SP_Search_Teacher_by_ID(
+@id int
+)
+AS
+BEGIN
+	SELECT*FROM VW_List_Teachers AS VW
+	WHERE VW.ID = @id
+END
+
+GO
+
+CREATE PROCEDURE SP_Update_Teacher(
+@teacher_id int,
+@lastname varchar(100),
+@names varchar(100),
+@dni int,
+@active bit
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			UPDATE Teachers
+			SET Lastname = @lastname,
+			Names = @names,
+			DNI = @dni,
+			Active = @active
+			WHERE ID = @teacher_id
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+		RAISERROR('No se pudo actualizar los datos del profesor.',16,1);
+	END CATCH
+END
+
+GO
+
+CREATE PROCEDURE SP_Search_Subject_by_ID( @subject_id int )
+AS
+BEGIN
+	SELECT*FROM VW_Subject_Info AS VW
+	WHERE VW.Subject_ID = @subject_id
+END
+
+GO
+
+CREATE PROCEDURE SP_List_Schedules_by_ID(
+	@subject_id int
+)
+AS
+BEGIN
+	SELECT*FROM Schedules WHERE Subject_ID = @subject_id
+	ORDER BY Day ASC, Start_Time ASC, End_Time ASC 
+END
+
+Exec SP_List_Schedules_by_ID 2
+
+GO
+
+CREATE PROCEDURE SP_Save_Schedule(
+	@subject_id int,
+	@day varchar(100),
+	@start_time time,
+	@end_time time
+)
+AS
+BEGIN
+	INSERT INTO Schedules(Subject_ID,Day,Start_Time,End_Time)
+	VALUES (@subject_id,@day,@start_time,@end_time)
+END
+
+GO
+
+CREATE PROCEDURE SP_Update_Subject(
+	@subject_id int,
+	@subject_name varchar(100),
+	@maximum_capacity int,
+	@old_teacher_id	int,
+	@new_teacher_id int
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			UPDATE Subjects
+			SET Subject_Name = @subject_name,
+			Maximum_Capacity = @maximum_capacity
+			WHERE ID = @subject_id
+			IF(@old_teacher_id = 0)
+				BEGIN
+					INSERT INTO Teachers_by_Subject(Subject_ID,Teacher_ID)
+					VALUES (@subject_id, @new_teacher_id)
+				END
+			ELSE
+				BEGIN
+					UPDATE Teachers_by_Subject
+					SET Teacher_ID = @new_teacher_id
+					WHERE Subject_ID = @subject_id AND
+					Teacher_ID = @old_teacher_id
+				END
+		COMMIT TRANSACTION
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+		RAISERROR('No se pudo actualizar los datos de la materia.',16,1);
+	END CATCH
+END
+
+GO
+
+CREATE PROCEDURE SP_Delete_Schedule(
+	@schedule_id int
+)
+AS
+BEGIN
+   DELETE FROM Schedules
+   WHERE ID = @schedule_id
+END
+
+GO
+
+CREATE PROCEDURE SP_Delete_Teacher_From_Subject(
+	 @subject_id int,
+	 @teacher_id int
+)
+AS
+BEGIN
+   DELETE FROM Teachers_by_Subject
+   WHERE Subject_ID = @subject_id AND
+   Teacher_ID = @teacher_id
+END
+
+GO
+
+CREATE VIEW	VW_List_Student
+AS
+select U.ID, S.Docket, PD.Lastname, PD.Names, PD.DNI, U.Administrator
+from Users as U
+join Personal_Data as PD on PD.User_ID = U.ID
+join Students as S on S.User_ID = U.ID
+
+GO
+
+CREATE VIEW VW_List_Administrators
+AS
+select U.ID, PD.Lastname, PD.Names,PD.DNI, U.Administrator
+from Users as U
+join Personal_Data as PD on PD.User_ID = U.ID
+join Administrator as A on A.User_ID = U.ID
+
+GO
+
+CREATE PROCEDURE SP_List_Ordered_Admin
+AS
+SELECT*FROM VW_List_Administrators AS VW
+ORDER BY VW.ID, VW.Lastname, VW.Names
+
+GO
+
+CREATE PROCEDURE SP_List_Ordered_Student
+AS
+SELECT*FROM VW_List_Student AS VW
+ORDER BY VW.Docket ASC, VW.Lastname ASC, VW.Names ASC
+
+
+GO
+
+CREATE PROCEDURE SP_Register_Student(
+	@dni int,
+	@lastname varchar(100),
+	@names varchar(100)
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+				INSERT INTO Users(Administrator)
+				VALUES ('0')
+				DECLARE @ult_user_id int
+				SET @ult_user_id = @@IDENTITY
+				INSERT INTO Students (User_ID)
+				VALUES (@ult_user_id)
+				INSERT INTO Personal_Data(User_ID,DNI,Lastname,Names)
+				VALUES(@ult_user_id, @dni,@lastname,@names)
+		COMMIT TRANSACTION
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+		RAISERROR('No se pudo registrar al estudiante.',16,1);
+	END CATCH
+END
+
+GO
+
+CREATE PROCEDURE SP_Register_Admin(
+	@password varchar(100),
+	@lastname varchar(100),
+	@names varchar(100),
+	@dni int
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			INSERT INTO Users(Administrator)
+			VALUES (1)
+			DECLARE @ult_user_id int
+			SET @ult_user_id = @@IDENTITY
+			INSERT INTO Administrator(User_ID, Password)
+			VALUES (@ult_user_id, @password)
+			INSERT INTO Personal_Data(User_ID, DNI, Lastname,Names)
+			VALUES(@ult_user_id, @dni,@lastname,@names)
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+		RAISERROR('No se pudo registrar al usuario administrador.',16,1);
+	END CATCH
+END
+
+-----------------------------------------------------------------------------------------------
+--                                      ALUMNO                                              ---
+
+GO
+
+CREATE PROCEDURE SP_Login_Student(
+	@docket int,
+	@dni int
+)
+AS
+BEGIN
+	Select U.ID, S.Docket, PD.Lastname, PD.Names, PD.DNI, U.Administrator
+	from Users as U
+	join Personal_Data as PD on PD.User_ID = U.ID
+	join Students as S on S.User_ID = U.ID
+	Where S.Docket = @docket AND
+	PD.DNI = @dni
+END
+
+GO
+
+CREATE VIEW VW_List_Subjects_Student
+AS
+SELECT S.ID, S.Subject_Name
+FROM Subjects AS S
+
+GO
+
+CREATE PROCEDURE SP_Enroll_in_a_Subject(
+	@subject_id int,
+	@student_docket int
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			INSERT INTO Inscriptions_by_Student(Subject_ID, Student_Docket,Enrollment_Date)
+			VALUES(@subject_id, @student_docket, GETDATE())
+			UPDATE Subjects
+			SET Places_Available = Places_Available-1
+			WHERE ID = @subject_id
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+		RAISERROR('No se pudo inscribir al usuario en la materia seleccionada.',16,1);
+	END CATCH
+END
+
+GO
+
+CREATE PROCEDURE SP_List_my_Enrollments(
+	@student_docket int
+)
+AS
+BEGIN
+	select S.Subject_Name, IbS.Enrollment_Date
+	from Inscriptions_by_Student as IbS
+	join Subjects as S on S.ID = IbS.Subject_ID
+	where IbS.Student_Docket = @student_docket
+END
+---------------------------------------------------------------------------
+
+--INSERT DE ALGUNOS DATOS DE PRUEBA
 GO
 INSERT INTO Users(Administrator)
 VALUES (1)
 GO
 INSERT INTO Administrator(User_ID,Password)
 VALUES (1,'admin') 
-GO
-INSERT INTO Subjects(Subject_Name,Maximum_Capacity)
-VALUES('Pro I',30,30)
-GO
-INSERT INTO Subjects(Subject_Name,Maximum_Capacity,Places_Available)
-VALUES('Lab III',90,90)
-GO
-INSERT INTO Teachers(Lastname,Names,DNI,Active)
-VALUES ('Lopez', 'Juan andres', 2020, 1)
-GO
-INSERT INTO Teachers_by_Subject(Subject_ID, Teacher_ID)
-VALUES(1,1)
-GO
-INSERT INTO Schedules(Subject_ID,Day,Start_Time,End_Time)
-VALUES(1, 'Martes', '21:00', '23:15')
-GO
-INSERT INTO Schedules(Subject_ID,Day,Start_Time,End_Time)
-VALUES(1, 'Viernes', '11:00', '12:30')
-GO
-INSERT INTO Schedules(Subject_ID,Day,Start_Time,End_Time)
-VALUES(2,'Domingo','08:00', '10:00')
-GO
-INSERT INTO Users(Administrator)
-VALUES ('0')
-GO
-DECLARE @ult_id int
-SET @ult_id = @@IDENTITY
-GO 
-INSERT INTO Students (User_ID, Docket)
-VALUES (@ult_id,)
+
+
+/*
+use master
+drop database AlkemyUniversity
+*/
---
+++ README.txt
@@ -0,0 +1,11 @@
+Aplición web que simula un pequeño sysacad de una universida.
+Los usuarios alumnos pueden ver e inscribirse a las materias que desean cursar.Los usuarios administradores pueden gestionar los prefesores y las materias a ofrecer.
+El lenguaje de programación principal utilizado de la aplicación es C# con el framework ASP.NET.  La base de datos que utilicé es Microsoft SQL Server 2019, haciendo las conexiones en el back mediante ADO.NET Entity Framework.
+Para las consultas utilizo queries, views, stored procedures con y sin parámetros y transacciones.
+Para el front utilicé el framework de CSS, Bootstrap 4.
+Para las validaciones utilicé JavaScript.
+-------------------------------------------------------------------
+*Nota*
+- La base de datos viene con un usuario administrador por defecto para loguearse:
+ID = 1
+Contraseña = "admin"
